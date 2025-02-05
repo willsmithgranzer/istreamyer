eb8717d77ee598e512ad7d5476391d45071f57ff
willsmithgranzer <91719351+willsmithgranzer@users.noreply.github.com>
2025-02-05 22:46:12 +0530

    Update leftad.js

--- js/leftad.js
+++ js/leftad.js
@@ -1,14 +1,4 @@
-// This script is designed to check if an ad blocker is active
-document.addEventListener("DOMContentLoaded", function () {
-    // Create a hidden div element that ad blockers may block
-    var adElement = document.createElement('div');
-    adElement.id = 'KQPgqSZstBRJ';
-    adElement.style.display = 'none';
-
-    // Try to append the element to the body
-    if (document.body) {
-        document.body.appendChild(adElement);
-    } else {
-        console.error("document.body is null; script executed too early.");
-    }
-});
+var e=document.createElement('div');
+e.id='KQPgqSZstBRJ';
+e.style.display='none';
+document.body.appendChild(e);
