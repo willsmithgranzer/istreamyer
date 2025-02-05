11ae373bac82a21c416321f4acfa68795808119a
willsmithgranzer <91719351+willsmithgranzer@users.noreply.github.com>
2025-02-05 22:17:48 +0530

    Create leftad.js

---
+++ js/leftad.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var e = document.createElement('div');
+    e.id = 'KQPgqSZstBRJ';
+    e.style.display = 'none';
+    
+    if (document.body) {
+        document.body.appendChild(e);
+    } else {
+        console.error("document.body is null; script executed too early.");
+    }
+});
